17262be39499437c1f712798eea9a67b5e87b79f
Yury Prime <yprimakov@gmail.com>
2022-12-28 03:42:30 +0000

    fix for #INC-5312: Missing image #1

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -20,6 +20,10 @@ const mapDispatchToProps = (dispatch) => ({
 const ItemPreview = (props) => {
   const item = props.item;
 
+  if (!item.image) {
+    item.image = "/placeholder.png";
+  }
+
   const handleClick = (ev) => {
     ev.preventDefault();
     if (item.favorited) {
